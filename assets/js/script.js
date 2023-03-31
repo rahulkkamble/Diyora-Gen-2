2e7ebd1a79b10dccc1a81bc586fb1a2562ac5d3c
faizalimulla <faizalimulla@gmail.com>
2023-03-31 17:49:20 +0530

    color and hover shape fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 // ======= SCROLL REVEAL ANIMATION =========
 const sr = ScrollReveal({  /* capital S in ScrollReveal */
     origin: 'bottom',
-    distance: '80px',
+    distance: '200px',
     duration: 1300,
     reset: false   /* for repeat animation */
 })
 
-sr.reveal(`.hero-image`)
+sr.reveal(`.hero-image`) /* , .get-started-heading, .why-section, .features-heading, .info-table */
 sr.reveal(`.box-image-inside.f1, .box-image-inside.f3`,{origin: 'right'})
 sr.reveal(`.box-image-inside.f2, .without-boy-img`,{origin: 'left'})
+// sr.reveal(`.footer-section`,{origin:'bottom', distance:'60px'})
 sr.reveal(` .boy-img`,{origin: 'right', delay:600})
