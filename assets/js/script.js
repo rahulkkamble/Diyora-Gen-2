9616ae5e5bf5b41a2da8f3a0ecd193a5e4047826
faizalimulla <faizalimulla@gmail.com>
2023-03-31 12:13:23 +0530

    given design is working properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,11 @@
 const sr = ScrollReveal({  /* capital S in ScrollReveal */
     origin: 'bottom',
     distance: '80px',
-    duration: 2000,
-    delay: 400,
+    duration: 1300,
     reset: true   /* for repeat animation */
 })
 
 sr.reveal(`.hero-image`)
 sr.reveal(`.box-image-inside.f1, .box-image-inside.f3`,{origin: 'right'})
 sr.reveal(`.box-image-inside.f2, .without-boy-img`,{origin: 'left'})
-sr.reveal(` .boy-img`,{origin: 'right', delay:1000})
+sr.reveal(` .boy-img`,{origin: 'right', delay:600})
