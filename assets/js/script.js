25f22cd1c8d68b6340c9ba356abd9fef133b654e
faizalimulla <faizalimulla@gmail.com>
2023-04-18 17:19:01 +0530

    new logo and animation part of getting started section, counting up numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,31 @@ sr.reveal(`.box-image-inside.f1, .box-image-inside.f3`,{origin: 'right'})
 sr.reveal(`.box-image-inside.f2, .without-boy-img`,{origin: 'left'})
 // sr.reveal(`.footer-section`,{origin:'bottom', distance:'60px'})
 sr.reveal(` .boy-img`,{origin: 'right', delay:600})
+
+// Counter
+var options = {
+    useEasing: true, 
+    useGrouping: true, 
+    separator: ',', 
+    decimal: '.', 
+};
+
+var counterParent = document.getElementById('unique-id');
+var children = counterParent.children;
+
+function countStart(){
+$('#unique-id').addClass('active');
+    for (var i = 0; i < children.length; i++) {
+         var item = document.getElementById(children[i].id).querySelector('.counter');
+         var countValue = item.getAttribute('data-value');
+
+        var countUP = new CountUp(item, 0, countValue, 0, 2, options);
+        if (!countUP.error) {
+            countUP.start();
+        }
+    }
+}
+
+ScrollReveal().reveal('.counter-block', { 
+    beforeReveal: countStart
+});	
